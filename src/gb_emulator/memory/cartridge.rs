8853d7bc689f92928226a53eda2cbda2969fdd87
Kolya Opahle <kolyaopahle@gmail.com>
2019-12-24 23:55:53 +0100

    implemented MBC3 and MBC5

--- src/gb_emulator/memory/cartridge.rs
+++ src/gb_emulator/memory/cartridge.rs
@@ -1,10 +1,10 @@
 #[derive(Debug)]
 enum MBCType {
-    MBC0,
+    NO,
     MBC1,
     MBC2,
     MBC3,
-    MBC5,
+    MBC5(u16),
 }
 
 pub struct Cartridge {
@@ -21,8 +21,8 @@ impl Cartridge {
             0x1 | 0x2 | 0x3 => MBCType::MBC1,
             0x5 | 0x6 => MBCType::MBC2,
             0xF | 0x10 | 0x11 | 0x12 | 0x13 => MBCType::MBC3,
-            0x19 | 0x1A | 0x1C | 0x1D | 0x1B | 0x1E => MBCType::MBC5,
-            _ => MBCType::MBC0,
+            0x19 | 0x1A | 0x1C | 0x1D | 0x1B | 0x1E => MBCType::MBC5(0),
+            _ => MBCType::NO,
         };
 
         Cartridge {
@@ -47,9 +47,42 @@ impl Cartridge {
 
     pub fn write_byte(&mut self, addr: u16, val: u8) {
         match self.memory_bank_type {
+            MBCType::NO => panic!(
+                "write attempt to cartridge without MBC: {:X} => {:X}",
+                addr, val
+            ),
             MBCType::MBC2 => match addr {
                 0x0000..=0x1FFF => self.ram_active = (addr & 0x100) == 0,
                 0x2000..=0x3FFF if (addr & 0x100) != 0 => self.select_rom_bank((val & 0xF) as u16),
+                _ => panic!("write to cartridge at read only address: {:X}", addr),
+            },
+            MBCType::MBC3 => match addr {
+                0x0000..=0x1FFF => self.ram_active = (val & 0xF) == 0xA,
+                0x2000..=0x3FFF => {
+                    if val == 0 {
+                        self.select_rom_bank(1)
+                    } else {
+                        self.select_rom_bank((val & 0x7F) as u16)
+                    }
+                }
+                0x4000..=0x7FFF => self.select_ram_bank(addr),
+                _ => panic!("write to cartridge at read only address: {:X}", addr),
+            },
+            MBCType::MBC5(ref mut addr_cache) => match addr {
+                0x0000..=0x1FFF => self.ram_active = (val & 0xF) == 0xA,
+                0x2000..=0x2FFF => {
+                    *addr_cache &= 0xFF00;
+                    *addr_cache |= val as u16;
+                    let rom_bank = *addr_cache;
+                    self.select_rom_bank(rom_bank);
+                }
+                0x3000..=0x3FFF => {
+                    *addr_cache &= 0xFF;
+                    *addr_cache |= ((val & 1) as u16) << 8;
+                    let rom_bank = *addr_cache;
+                    self.select_rom_bank(rom_bank);
+                }
+                0x4000..=0x5FFF => self.select_ram_bank(val as u16),
                 _ => panic!("write to cartridge at read only address: {:X}", addr),
             },
             _ => panic!(
@@ -64,8 +97,12 @@ impl Cartridge {
 
         assert!(
             self.cartridge_buffer.len() >= self.swap_rom_offset + 0x4000,
-            "Tried to swap in a bank not available on rom: {:X}",
+            "tried to swap in a bank not available on rom: {:X}",
             self.swap_rom_offset
         );
+    }
+
+    fn select_ram_bank(&mut self, bank_id: u16) {
+        panic!("rom bank selection not implemented");
     }
 }
