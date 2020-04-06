992c73388df91d563e0dd578bcffcef5abd8eace
Kolya Opahle <kolyaoaphle@gmail.com>
2020-04-06 10:41:06 +0200

    coole threads

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,48 @@
 mod gb_emulator;
 use pixels::{wgpu::Surface, Pixels, SurfaceTexture};
-use winit::{event_loop::EventLoop, window::WindowBuilder};
+use std::thread;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let surface = Surface::create(&window);
-    let size = window.inner_size();
-    let width = size.width;
-    let height = size.height;
-    let surface_texture = SurfaceTexture::new(width, height, surface);
-    let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
 
-    loop {
-        let frame = pixels.get_frame();
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            pixel[0] = (i % 0xff) as u8; // R
-            pixel[1] = 0xff; // G
-            pixel[2] = 0xff; // B
-            pixel[3] = 0xff; // A
+    thread::spawn(move || loop {
+        let surface = Surface::create(&window);
+        let size = window.inner_size();
+        let width = size.width;
+        let height = size.height;
+        let surface_texture = SurfaceTexture::new(width, height, surface);
+        let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
+
+        loop {
+            let frame = pixels.get_frame();
+            for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+                pixel[0] = (i % 0xff) as u8;
+                pixel[1] = (i / 2 % 0xff) as u8;
+                pixel[2] = (i % 0xff) as u8;
+                pixel[3] = 0xff;
+            }
+
+            pixels.render();
         }
+    });
 
-        pixels.render();
-    }
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 
     gb_emulator::start_emulation();
 }
