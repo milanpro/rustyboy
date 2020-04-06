db9e9c6e21dd9206159d7246673867b4dc68769c
Kolya Opahle <kolyaoaphle@gmail.com>
2020-04-06 10:16:34 +0200

    can now create a window and draw an empty pixel buffer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 mod gb_emulator;
+use pixels::{wgpu::Surface, Pixels, SurfaceTexture};
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let surface = Surface::create(&window);
+    let size = window.inner_size();
+    let width = size.width;
+    let height = size.height;
+    let surface_texture = SurfaceTexture::new(width, height, surface);
+    let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
+
+    loop {
+        pixels.render();
+    }
+
     gb_emulator::start_emulation();
 }
