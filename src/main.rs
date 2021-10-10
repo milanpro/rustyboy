ab839af6c0201a90f7cbb1c3d793e844b70192f5
Milan Proell <milan.proell@googlemail.com>
2021-10-10 14:48:58 +0200

    Update dependencies
    Co-authored-by: kolyaopahle@gmail.com <kolyaopahle@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 mod gb_emulator;
+use log::error;
 use interpolation::Lerp;
-use pixels::{wgpu::Surface, Pixels, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
+use core::panic;
 use std::thread;
 use winit::{
     dpi::PhysicalSize,
-    event::{Event, WindowEvent},
+    event::{Event, VirtualKeyCode},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+use winit_input_helper::WinitInputHelper;
+
 fn main() {
+    env_logger::init();
     let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
     let window = WindowBuilder::new()
         .with_inner_size(PhysicalSize::new(800, 600))
         .build(&event_loop)
         .unwrap();
 
-    thread::spawn(move || loop {
-        let surface = Surface::create(&window);
         let size = window.inner_size();
         let width = size.width;
         let height = size.height;
-        let surface_texture = SurfaceTexture::new(width, height, surface);
-        let mut pixels = Pixels::new(width, height, surface_texture).unwrap();
+        let surface_texture = SurfaceTexture::new(width, height, &window);
+    let mut pixels = match Pixels::new(width, height, surface_texture) {
+        Ok(pixels) => pixels,
+        Err(error) => {
+            error!("Creating pixels backend failed: {}", error);
+            panic!();
+        }
+    };
 
-        loop {
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
             let frame = pixels.get_frame();
             for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
                 pixel[0] =
@@ -32,22 +43,31 @@ fn main() {
                 pixel[2] = 0;
                 pixel[3] = 0xff;
             }
+            if pixels
+                .render()
+                .map_err(|e| error!("pixels.render() failed: {}", e))
+                .is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
 
-            pixels.render();
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                pixels.resize_surface(size.width, size.height);
+            }
+
+            window.request_redraw();
         }
     });
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
-
-    gb_emulator::start_emulation();
+    //gb_emulator::start_emulation();
 }
