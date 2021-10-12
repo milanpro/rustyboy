d58d8a5a069daeaaf7d9dfd586909698d4d68a87
Kolya Opahle <kolyaopahle@gmail.com>
2021-10-12 16:26:54 +0200

    moved opcode implementations out of cpu file

--- src/gb_emulator/cpu/mod.rs
+++ src/gb_emulator/cpu/mod.rs
@@ -1,8 +1,9 @@
-use crate::gb_emulator::registers::Flag;
+mod opcodes;
+
+use opcodes::Opcodes;
 
 use super::memory::MemoryBus;
 use super::registers::Registers;
-use super::utils::U16Ext;
 
 pub struct Z80CPU {
     r: Registers,
@@ -49,92 +50,6 @@ impl Z80CPU {
         let w = (b as u16) << 8 + self.m.read_byte(self.r.pc);
         self.r.pc += 1;
         w
-    }
-
-    fn add(&mut self, val: u8) {
-        let res = (self.r.a as u16) + (val as u16);
-
-        self.r.set_flag(Flag::Z, res.lo() == 0);
-        self.r
-            .set_flag(Flag::H, ((self.r.a & 0x0F) + (val & 0x0F)) & 0x10 == 0x10);
-        self.r.set_flag(Flag::C, res > 0xFF);
-        self.r.set_flag(Flag::N, false);
-
-        self.r.a = res.lo();
-    }
-
-    fn adc(&mut self, val: u8) {
-        let mut res = (self.r.a as u16) + (val as u16);
-
-        if self.r.get_flag(Flag::C) {
-            res += 1;
-        }
-
-        self.r.set_flag(Flag::Z, res.lo() == 0);
-        self.r
-            .set_flag(Flag::H, ((self.r.a & 0x0F) + (val & 0x0F)) & 0x10 == 0x10);
-        self.r.set_flag(Flag::C, res > 0xFF);
-        self.r.set_flag(Flag::N, false);
-
-        self.r.a = res.lo();
-    }
-
-    fn sub(&mut self, val: u8) {
-        self.cp(val);
-        self.r.a -= val;
-    }
-
-    fn sbc(&mut self, val: u8) {
-        let res = self.r.a - val - 1;
-
-        self.r
-            .set_flag(Flag::H, ((self.r.a & 0xF) as i8 - (val & 0xF) as i8) < 0);
-        self.r
-            .set_flag(Flag::C, ((self.r.a as i16) - (val as i16) - 1) < 0);
-        self.r.set_flag(Flag::N, true);
-        self.r.set_flag(Flag::Z, res == 0);
-
-        self.r.a = res;
-    }
-
-    fn and(&mut self, val: u8) {
-        let res = self.r.a & val;
-
-        self.r.set_flag(Flag::Z, res == 0);
-        self.r.set_flag(Flag::H, true);
-        self.r.set_flag(Flag::N, false);
-        self.r.set_flag(Flag::C, false);
-
-        self.r.a = res;
-    }
-
-    fn xor(&mut self, val: u8) {
-        let res = self.r.a ^ val;
-
-        self.r.set_flag(Flag::Z, res == 0);
-        self.r.set_flag(Flag::H, false);
-        self.r.set_flag(Flag::N, false);
-        self.r.set_flag(Flag::C, false);
-
-        self.r.a = res;
-    }
-
-    fn or(&mut self, val: u8) {
-        let res = self.r.a | val;
-
-        self.r.set_flag(Flag::Z, res == 0);
-        self.r.set_flag(Flag::H, false);
-        self.r.set_flag(Flag::N, false);
-        self.r.set_flag(Flag::C, false);
-
-        self.r.a = res;
-    }
-
-    fn cp(&mut self, val: u8) {
-        self.r.set_flag(Flag::Z, self.r.a == val);
-        self.r.set_flag(Flag::H, (self.r.a & 0xF) < (val & 0xF));
-        self.r.set_flag(Flag::N, true);
-        self.r.set_flag(Flag::C, self.r.a < val);
     }
 
     // fetch and run the next instruction, returns the length of the ran instruction
---
+++ src/gb_emulator/cpu/opcodes.rs
@@ -0,0 +1,102 @@
+use crate::gb_emulator::cpu::Z80CPU;
+use crate::gb_emulator::registers::Flag;
+use crate::gb_emulator::utils::U16Ext;
+
+pub trait Opcodes {
+    fn add(&mut self, val: u8);
+    fn adc(&mut self, val: u8);
+    fn sub(&mut self, val: u8);
+    fn sbc(&mut self, val: u8);
+    fn and(&mut self, val: u8);
+    fn xor(&mut self, val: u8);
+    fn or(&mut self, val: u8);
+    fn cp(&mut self, val: u8);
+}
+
+impl Opcodes for Z80CPU {
+    fn add(&mut self, val: u8) {
+        let res = (self.r.a as u16) + (val as u16);
+
+        self.r.set_flag(Flag::Z, res.lo() == 0);
+        self.r
+            .set_flag(Flag::H, ((self.r.a & 0x0F) + (val & 0x0F)) & 0x10 == 0x10);
+        self.r.set_flag(Flag::C, res > 0xFF);
+        self.r.set_flag(Flag::N, false);
+
+        self.r.a = res.lo();
+    }
+
+    fn adc(&mut self, val: u8) {
+        let mut res = (self.r.a as u16) + (val as u16);
+
+        if self.r.get_flag(Flag::C) {
+            res += 1;
+        }
+
+        self.r.set_flag(Flag::Z, res.lo() == 0);
+        self.r
+            .set_flag(Flag::H, ((self.r.a & 0x0F) + (val & 0x0F)) & 0x10 == 0x10);
+        self.r.set_flag(Flag::C, res > 0xFF);
+        self.r.set_flag(Flag::N, false);
+
+        self.r.a = res.lo();
+    }
+
+    fn sub(&mut self, val: u8) {
+        self.cp(val);
+        self.r.a -= val;
+    }
+
+    fn sbc(&mut self, val: u8) {
+        let res = self.r.a - val - 1;
+
+        self.r
+            .set_flag(Flag::H, ((self.r.a & 0xF) as i8 - (val & 0xF) as i8) < 0);
+        self.r
+            .set_flag(Flag::C, ((self.r.a as i16) - (val as i16) - 1) < 0);
+        self.r.set_flag(Flag::N, true);
+        self.r.set_flag(Flag::Z, res == 0);
+
+        self.r.a = res;
+    }
+
+    fn and(&mut self, val: u8) {
+        let res = self.r.a & val;
+
+        self.r.set_flag(Flag::Z, res == 0);
+        self.r.set_flag(Flag::H, true);
+        self.r.set_flag(Flag::N, false);
+        self.r.set_flag(Flag::C, false);
+
+        self.r.a = res;
+    }
+
+    fn xor(&mut self, val: u8) {
+        let res = self.r.a ^ val;
+
+        self.r.set_flag(Flag::Z, res == 0);
+        self.r.set_flag(Flag::H, false);
+        self.r.set_flag(Flag::N, false);
+        self.r.set_flag(Flag::C, false);
+
+        self.r.a = res;
+    }
+
+    fn or(&mut self, val: u8) {
+        let res = self.r.a | val;
+
+        self.r.set_flag(Flag::Z, res == 0);
+        self.r.set_flag(Flag::H, false);
+        self.r.set_flag(Flag::N, false);
+        self.r.set_flag(Flag::C, false);
+
+        self.r.a = res;
+    }
+
+    fn cp(&mut self, val: u8) {
+        self.r.set_flag(Flag::Z, self.r.a == val);
+        self.r.set_flag(Flag::H, (self.r.a & 0xF) < (val & 0xF));
+        self.r.set_flag(Flag::N, true);
+        self.r.set_flag(Flag::C, self.r.a < val);
+    }
+}
