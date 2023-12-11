5a90008be2b9cf76c0eca5065f21f1f0e55f0610
Milan Proell <m.proell@sap.com>
2023-12-11 15:18:03 +1100

    Update dependecies

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ use pixels::{Pixels, SurfaceTexture};
 use rustyboy::start_emulation;
 use winit::{
     dpi::PhysicalSize,
-    event::{Event, VirtualKeyCode},
-    event_loop::{ControlFlow, EventLoop},
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::KeyCode,
     window::WindowBuilder,
 };
 use winit_input_helper::WinitInputHelper;
 
 fn main() {
     env_logger::init();
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new().unwrap();
     let mut input = WinitInputHelper::new();
     let window = WindowBuilder::new()
         .with_inner_size(PhysicalSize::new(800, 600))
@@ -34,46 +35,56 @@ fn main() {
 
     start_emulation();
 
-    event_loop.run(move |event, _, control_flow| {
-        if let Event::RedrawRequested(_) = event {
-            let win_width = window.inner_size().width;
-            let frame = pixels.frame_mut();
-            for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-                let r: u8 = 0x0.lerp(&0xff, &((i as u32 % win_width) as f32 / win_width as f32));
-                pixel.copy_from_slice(&[r, 0, 0, 0xff]);
-            }
-            if pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e))
-                .is_err()
+    event_loop
+        .run(move |event, elwt| {
+            if let Event::WindowEvent {
+                event: window_event,
+                ..
+            } = &event
             {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-        // Handle input events
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape)
-                || input.close_requested()
-                || input.destroyed()
-            {
-                *control_flow = ControlFlow::Exit;
-                return;
+                if window_event == &WindowEvent::RedrawRequested {
+                    let win_width = window.inner_size().width;
+                    let frame = pixels.frame_mut();
+                    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+                        let r: u8 =
+                            0x0.lerp(&0xff, &((i as u32 % win_width) as f32 / win_width as f32));
+                        pixel.copy_from_slice(&[r, 0, 0, 0xff]);
+                    }
+                    if pixels
+                        .render()
+                        .map_err(|e| error!("pixels.render() failed: {}", e))
+                        .is_err()
+                    {
+                        elwt.exit();
+                        return;
+                    }
+                }
             }
 
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                if let Err(err) = pixels
-                    .resize_surface(size.width, size.height)
-                    .and_then(|_| pixels.resize_buffer(size.width, size.height))
+            // Handle input events
+            if input.update(&event) {
+                // Close events
+                if input.key_pressed(KeyCode::Escape)
+                    || input.close_requested()
+                    || input.destroyed()
                 {
-                    error!("pixels.resize_surface {:?}", err);
-                    *control_flow = ControlFlow::Exit;
+                    elwt.exit();
                     return;
                 }
-                window.request_redraw();
+
+                // Resize the window
+                if let Some(size) = input.window_resized() {
+                    if let Err(err) = pixels
+                        .resize_surface(size.width, size.height)
+                        .and_then(|_| pixels.resize_buffer(size.width, size.height))
+                    {
+                        error!("pixels.resize_surface {:?}", err);
+                        elwt.exit();
+                        return;
+                    }
+                    window.request_redraw();
+                }
             }
-        }
-    });
+        })
+        .unwrap();
 }
